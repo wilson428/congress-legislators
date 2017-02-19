4bb53d62e821d8a41e0807893ac9389d96e7cd31
Joel Collins <jconline@gmail.com>
2017-02-19 13:12:47 -0500

    add leaderships to sanity check output

--- scripts/committee_membership_sanity_check.py
+++ scripts/committee_membership_sanity_check.py
@@ -8,12 +8,16 @@ def run():
   subcount = 0
   last = ""
   for name, members in memberships_current.items():
+    leaderships = []
+    for member in members:
+      if 'title' in member:
+        leaderships.append(member["title"])
     if name[:4] == last:
       subcount+=1
-      print("{} : {}".format(" " + name.ljust(6), len(members)))
+      print("{} : {} | {}".format(" " + name.ljust(6), str(len(members)).ljust(2), ", ".join(sorted(leaderships))))
     else:
       maincount+=1
-      print("{} : {}".format(name.ljust(7), len(members)))
+      print("{} : {} | {}".format(name.ljust(7), str(len(members)).ljust(2), ", ".join(sorted(leaderships))))
     last=name[:4]
 
   print("There are {} total entries, {} committees and {} subcommittees.".format(len(memberships_current), maincount, subcount))
